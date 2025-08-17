c37d57ca46f50409a37f55f9d613c326bf411736
vincent71399 <vincent71399@gmail.com>
2025-08-17 19:22:56 +0800

    -add deploy nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: "",
+    assetPrefix: "./",
+    trailingSlash: true,
 };
 
 export default nextConfig;
